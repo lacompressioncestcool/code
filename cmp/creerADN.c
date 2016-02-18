dbcebabce6a791b8f967b9f8b5207c03aec82789
SuzanneAir <suzanneair@live.fr>
2016-02-18 15:34:12 +0100

    modifications de creerADN

--- cmp/creerADN.c
+++ cmp/creerADN.c
@@ -1,4 +1,38 @@
-maine
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <time.h>
+
+float alea_float()
+	{
+	static int init_alea = 1;
+	if (init_alea) srand(getpid());
+	init_alea = 0;
+	return (float)rand() / (float)RAND_MAX;
+	}
+
+int alea_int(int N) { return (int)(N*alea_float()); }
+
+
+int main()
 {
-  
+
+    int a;
+    int n;
+    int i;
+
+    scanf("%d \n",&n);
+
+    FILE* fichier = fopen("ADN.txt","w");
+
+    for(i = 0; i < n; i++)
+    {
+        a = alea_int(4);
+        if(a == 0) { fprintf(fichier,"A"); }
+        else if(a == 1) { fprintf(fichier,"T"); }
+        else if(a == 2) { fprintf(fichier,"G"); }
+        else { fprintf(fichier,"C"); }
+    }
+
+    return 0;
 }
